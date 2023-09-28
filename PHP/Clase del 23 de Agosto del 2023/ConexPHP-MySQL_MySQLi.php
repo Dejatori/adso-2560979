35b9d2211ee94ef435d9c37d8035b4bfa9517ef3
Dejatori <deivitsc@gmail.com>
2023-09-27 20:56:10 -0500

    Comentarios explicativos

--- PHP/Clase del 23 de Agosto del 2023/ConexPHP-MySQL_MySQLi.php
+++ PHP/Clase del 23 de Agosto del 2023/ConexPHP-MySQL_MySQLi.php
@@ -1,15 +1,19 @@
+<!-- Este archivo es de conexión a la base de datos usando mysqli -->
+
 <?php 
 $host = 'localhost';
 $dbname = 'prueba';
 $username = 'root';
 $password = '';
 
-// Create connection
-$conn = mysqli_connect($host, $username, $password, $dbname);
-// Check connection
+// Crear la conexión con mysqli 
+
+$conn = mysqli_connect($host, $username, $password, $dbname); // variable de conexión que se llama $conn y se le asigna la función mysqli_connect 
+// que recibe los parámetros de conexión que son el host, el usuario, la contraseña y el nombre de la base de datos
+// Verificar la conexión
 if (!$conn) {
     die("Conexión Fallida: " . mysqli_connect_error());
-}
-echo "Gracias a Dios nos conectamos";
-mysqli_close($conn);
+}  // si la conexión falla, se ejecuta el mensaje de error y se termina la ejecución del programa
+echo "Gracias a Dios nos conectamos"; // si la conexión es exitosa, se ejecuta el mensaje de conexión exitosa
+mysqli_close($conn); // se cierra la conexión
 ?>
--- PHP/Clase del 23 de Agosto del 2023/ConexPHP-MySQL_PDO.php
+++ PHP/Clase del 23 de Agosto del 2023/ConexPHP-MySQL_PDO.php
@@ -1,20 +1,22 @@
+<!-- Archivo PHP que realiza la conexión a la base de datos usando PDO -->
+
 <html>
  <head>
   <title>Prueba de PHP</title>
  </head>
  <body>
-
  
 <?php
-include_once("DatosMySQL.php");
- //$conn=Conexion::ConexionBD();
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "Gracias a Dios: conectado a $dbname en $host Exitosamente.";
-} catch (PDOException $pe) {
-    die("Nada de Conexión $dbname :" . $pe->getMessage());
+include_once("DatosMySQL.php"); // incluir el archivo DatosMySQL.php que contiene los datos de conexión a la base de datos
+
+try { // try es un manejador de excepciones que intenta ejecutar el código que está dentro de él, si no puede, ejecuta el catch
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); // variable de conexión que se llama $conn y se le asigna la función PDO 
+    // que recibe los parámetros de conexión que son el host, el usuario, la contraseña y el nombre de la base de datos que están en el archivo DatosMySQL.php
+    echo "Gracias a Dios: conectado a $dbname en $host Exitosamente."; // si la conexión es exitosa, se ejecuta el mensaje de conexión exitosa
+} catch (PDOException $pe) { // catch es un manejador de excepciones que captura el error que se genera en el try y lo guarda en la variable $pe
+    die("Nada de Conexión $dbname :" . $pe->getMessage()); // si la conexión falla, se ejecuta el mensaje de error y se termina la ejecución del programa
 }
- $conn = null;
+ $conn = null; // se cierra la conexión
 ?>
 
 </body>
